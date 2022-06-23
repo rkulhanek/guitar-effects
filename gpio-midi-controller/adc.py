58d9b1117da2e2e7512591c71a0aa04bd73dd02d
Ray Kulhanek <raymond.kulhanek@wright.edu>
2022-06-22 21:56:53 -0400

    added second expression pedal

--- gpio-midi-controller/adc.py
+++ gpio-midi-controller/adc.py
@@ -14,9 +14,11 @@ def ohms(v: float) -> float:
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 cs = digitalio.DigitalInOut(board.D22)
 mcp = MCP.MCP3008(spi, cs)
-chan = AnalogIn(mcp, MCP.P0)
+chan6 = AnalogIn(mcp, MCP.P6)
+chan7 = AnalogIn(mcp, MCP.P7)
 
-window = [ 0.05 ] * 60
+N = 30
+window = [ 0.05 ] * (N*2)
 window[1] = 22000
 plt.ion()
 fig = plt.figure()
@@ -29,9 +31,11 @@ fig.canvas.flush_events()
 fig.canvas.mpl_connect('close_event', exit)
 
 while True:
-	window = [ ohms(chan.voltage) ] * 60
+	window = [ ohms(chan6.voltage) ] * N + [ ohms(chan7.voltage) ] * N
 	line1.set_ydata(window)
-	plt.title(f"{chan.value:5d} :: {chan.voltage:.2f}V :: {ohms(chan.voltage):5.0f}Ω")
+	plt.title(
+		f"{chan6.value:5d} :: {chan6.voltage:.2f}V :: {ohms(chan6.voltage):5.0f}Ω\n"
+		+ f"{chan7.value:5d} :: {chan7.voltage:.2f}V :: {ohms(chan7.voltage):5.0f}Ω")
 	fig.canvas.draw()
 	fig.canvas.flush_events()
 	time.sleep(0.05)
