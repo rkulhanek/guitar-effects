d07a4ff30f02dd92d77e661a3239e24c92905e55
Ray Kulhanek <raymond.kulhanek@wright.edu>
2022-06-22 21:57:12 -0400

    equalize base volume across clean, dirty, nevermore-drive

--- guitarix-config/banks/banklist.js
+++ guitarix-config/banks/banklist.js
@@ -1,3 +1,14 @@
 [
-  ["Scratchpad", "scratchpad.gx", 0, 0, [1, 2], 1654967468]
+  ["f", "f.gx", 1, 0, [1, 2], 1655849976],
+  ["VoxBeatleMysteryPreset", "VoxBeatleMysteryPreset.gx", 1, 0, [1, 2], 1655849778],
+  ["Nevermore", "Nevermore.gx", 1, 0, [1, 2], 1655845502],
+  ["P.Z.", "P%2eZ%2e.gx", 1, 0, [1, 2], 1655845391],
+  ["running_wild", "running_wild.gx", 1, 0, [1, 2], 1655845356],
+  ["hk", "hk.gx", 1, 0, [1, 2], 1655845296],
+  ["journey", "journey.gx", 1, 0, [1, 2], 1655845189],
+  ["silentz0r", "silentz0r.gx", 1, 0, [1, 2], 1655845163],
+  ["AJFA", "AJFA.gx", 1, 0, [1, 2], 1655845126],
+  ["80s_Hard_n_Heavy", "80s_Hard_n_Heavy.gx", 1, 0, [1, 2], 1655845092],
+  ["metalpack-v1", "metalpack-v1.gx", 1, 0, [1, 2], 1655844965],
+  ["Scratchpad", "scratchpad.gx", 0, 0, [1, 2], 1655949373]
 ]
