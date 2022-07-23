9e9cd3a77de13cc0e23ffa9a03a52aa638436d23
Ray Kulhanek <raymond.kulhanek@wright.edu>
2022-07-23 19:36:57 -0400

    adc.py works for both rheostats

--- gpio-midi-controller/adc.py
+++ gpio-midi-controller/adc.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-import time
+import time, atexit
 import busio
 import digitalio
 import board
+import RPi.GPIO as gpio
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 import matplotlib.pyplot as plt
@@ -11,31 +12,48 @@ import matplotlib.pyplot as plt
 def ohms(v: float) -> float:
 	return (51000 * v) / (33.0 - 10.0 * v)
 
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-cs = digitalio.DigitalInOut(board.D22)
-mcp = MCP.MCP3008(spi, cs)
-chan6 = AnalogIn(mcp, MCP.P6)
-chan7 = AnalogIn(mcp, MCP.P7)
+minval = 13300
+maxval = 63040
+def val01(v: int) -> float:
+	v = float(v) - minval
+	if v < 0.0:
+		v = 0.0
+	return v / (maxval - minval)
 
-N = 30
-window = [ 0.05 ] * (N*2)
-window[1] = 22000
-plt.ion()
-fig = plt.figure()
-ax = fig.add_subplot(111)
-line1, = ax.plot(window)
+atexit.register(gpio.cleanup)
 
-line1.set_ydata(window)
-fig.canvas.draw()
-fig.canvas.flush_events()
-fig.canvas.mpl_connect('close_event', exit)
 
-while True:
-	window = [ ohms(chan6.voltage) ] * N + [ ohms(chan7.voltage) ] * N
+try:
+	print('Try')
+	spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+	cs = digitalio.DigitalInOut(board.D0)
+	mcp = MCP.MCP3008(spi, cs)
+	chan6 = AnalogIn(mcp, MCP.P6)
+	chan7 = AnalogIn(mcp, MCP.P7)
+
+	N = 30
+	window = [ 0.01 ] * (N*2)
+	window[1] = 1.0
+	plt.ion()
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	line1, = ax.plot(window)
+
 	line1.set_ydata(window)
-	plt.title(
-		f"{chan6.value:5d} :: {chan6.voltage:.2f}V :: {ohms(chan6.voltage):5.0f}Ω\n"
-		+ f"{chan7.value:5d} :: {chan7.voltage:.2f}V :: {ohms(chan7.voltage):5.0f}Ω")
 	fig.canvas.draw()
 	fig.canvas.flush_events()
-	time.sleep(0.05)
+	fig.canvas.mpl_connect('close_event', exit)
+
+	while True:
+		window = [ val01(chan6.value) ] * N + [ val01(chan7.value) ] * N
+		line1.set_ydata(window)
+		plt.title(
+			f"{chan6.value:5d} :: {chan6.voltage:.2f}V :: {ohms(chan6.voltage):5.0f}Ω :: {val01(chan6.value):.6f}\n"
+			+ f"{chan7.value:5d} :: {chan7.voltage:.2f}V :: {ohms(chan7.voltage):5.0f}Ω :: {val01(chan7.value):.6f}")
+		fig.canvas.draw()
+		fig.canvas.flush_events()
+		time.sleep(0.05)
+except KeyboardInterrupt:
+	pass
+
+
