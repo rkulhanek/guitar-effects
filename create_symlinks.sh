c5e1d81aa9f8b08ba1b80e703b612b0967f6c392
Ray Kulhanek <raymond.kulhanek@wright.edu>
2022-07-24 15:10:57 -0400

    create_symlinks adds autostart desktop entry

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+DIR=$(dirname $(realpath $0))
+cd "$DIR"
+
+# TODO: add all the apt install stuff here, compile start.d, etc. Then rename to install.sh
+
 ln -s $(realpath jackdrc) ~/.jackdrc
-mkdir -p ~/.config
+mkdir -p ~/.config/autostart
 ln -s $(realpath guitarix-config) ~/.config/guitarix
+
+AUTOSTART="$HOME/.config/autostart/start.desktop"
+cat > "$AUTOSTART" <<-EOF
+	[Desktop Entry]
+	Encoding=UTF-8
+	Name=Start
+	Type=Application
+	Exec=$DIR/start
+EOF
+chmod u+x "$AUTOSTART"
