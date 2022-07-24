3c1878fcf05b32f794bfbad3733f85ec9bdb56ae
Ray Kulhanek <raymond.kulhanek@wright.edu>
2022-07-23 20:03:15 -0400

    merged adc code into midi.py

--- gpio-midi-controller/midi.py
+++ gpio-midi-controller/midi.py
@@ -8,9 +8,13 @@
 # of some kind; jack's midi monitor program prints it differently.
 # 64 works.
 
-import rtmidi, time, sys, time
+import rtmidi, time, sys, time, atexit, busio, board, digitalio
 import RPi.GPIO as gpio
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
 from collections import namedtuple
+
+atexit.register(gpio.cleanup)
 
 MIDI_CHANNEL = 0
 assert 0 <= MIDI_CHANNEL <= 0xF
@@ -32,11 +36,12 @@ skip_next = { k : False for k in TOGGLE_PEDAL.keys() }
 midi_out = None
 
 def status(color: str) -> None:
+	# TODO: actually use this. Monitor the jackd logfile. Yellow if we've got recent xruns, red on error
 	if 'red' == color:
 		gpio.output(27, 0)
 		gpio.output(22, 0)
 		gpio.output(17, 1)
-	elif 'yellow' == color:
+	if 'yellow' == color:
 		gpio.output(17, 0)
 		gpio.output(22, 0)
 		gpio.output(27, 1)
@@ -44,10 +49,20 @@ def status(color: str) -> None:
 		gpio.output(17, 0)
 		gpio.output(27, 0)
 		gpio.output(22, 1)
-	elif 'test' == color:
-		gpio.output(17, 1)
-		gpio.output(27, 1)
-		gpio.output(22, 1)
+	else:
+		assert(0)
+
+def adc01(chan: AnalogIn) -> float:
+	""" Returns value of MCP3008 channel `chan` normalized to [0,1] """
+	MIN_VAL = 13300 # These were measured with a 5.1k resistor wired into the circuit.
+	MAX_VAL = 63040 # They varied a bit between the two pedals, but not by enough to matter
+
+	# TODO: maybe introduce a nonlinearity; it requires more force to move the first 50% than the second.
+	# But see how it sounds before messing with it.
+	v = float(chan.value) - MIN_VAL
+	if v < 0.0:
+		v = 0.0
+	return v / (MAX_VAL - MIN_VAL)
 
 def toggle(pin: int) -> None:
 	# We see a rising edge when it's pressed *and* when it's released
@@ -65,16 +80,16 @@ def toggle(pin: int) -> None:
 	skip_next[pin] = not skip_next[pin]
 
 def setup_gpio() -> None:
-	global state
+	global state, adc_chan6, adc_chan7
 	gpio.setmode(gpio.BCM)
 	#gpio.setup(16, gpio.OUT)
+
 	# RYG pins
 	gpio.setup(17, gpio.OUT)
 	gpio.setup(22, gpio.OUT)
 	gpio.setup(27, gpio.OUT)
-	gpio.setup(POWER_LIGHT, gpio.OUT)
-	gpio.output(POWER_LIGHT, 1)
 
+	# Toggle pedals
 	for pin in TOGGLE_PEDAL.keys():
 		gpio.setup(pin, gpio.IN, pull_up_down=gpio.PUD_UP)
 		gpio.setup(TOGGLE_PEDAL[pin].led, gpio.OUT)
@@ -98,6 +113,18 @@ def setup_gpio() -> None:
 		state[k] = not state # Lets us initialize state with the desired values instead of inverse
 		toggle(k)
 		skip_next[k] = False
+	
+	# Power button
+	gpio.setup(POWER_LIGHT, gpio.OUT)
+	gpio.output(POWER_LIGHT, 1) # LED
+	gpio.setup(3, gpio.IN) # Button
+
+	# MCP3008 ADC
+	spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+	cs = digitalio.DigitalInOut(board.D0)
+	mcp = MCP.MCP3008(spi, cs)
+	adc_chan6 = AnalogIn(mcp, MCP.P6)
+	adc_chan7 = AnalogIn(mcp, MCP.P7)
 
 def setup_midi() -> None:
 	global midi_out
@@ -111,24 +138,18 @@ try:
 	sys.stderr.write("setup_gpio\n")
 	sys.stderr.flush();
 	setup_gpio()
-	gpio.setup(3, gpio.IN)
-	print("ready")
-	sys.stderr.write("ready\n")
-	sys.stderr.flush();
 	status('green')
+	print("ready")
 	while True:
-#		for p in TOGGLE_PEDAL.keys():
-#			print(f"{p}: {gpio.input(p)}")
-#		print(gpio.input(5))
-#		pins = [ gpio.input(p) for p in TOGGLE_PEDAL.keys() ]
-	#	if 4 != sum(pins):
-#		print(''.join([ f"{p}" for p in pins ]), gpio.input(3))
-#		time.sleep(0.5)
-		time.sleep(200)
+		# TODO: see if preset toggle switches have changed
+		# TODO: shutdown if power button off (be sure to call gpio.cleanup()
+		# TODO: if adc difference exceeds (small) threshold, send midi signal
+		# TODO: when toggle pedals pressed, send signal
+		print(f"{adc01(adc_chan6):.6f} --- {adc01(adc_chan7):.6f}")
+		time.sleep(0.1)
 except KeyboardInterrupt:
 	# TODO: make this happen on shutdown, too. Trivial once
 	# all the code is in the one script.
-	gpio.cleanup()
 	if midi_out:
 		midi_out.delete()
 		del midi_out
